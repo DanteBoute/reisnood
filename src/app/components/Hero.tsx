257147bece13f50f83157ce81b3f29ac9dd8c540
DanteBoute <danteboute1@outlook.com>
2024-04-11 09:35:52 +0200

    Change hero photo's

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import './Hero.css'; // Adjust the import statement
+import './Hero.css';
+import carrouselOne from "../../../public/images/IMG_1974 kopie 5.png"
+import carrouselTwo from "../../../public/images/lightroom italie 2023 - 1 van 1.jpeg"
+import carrouselThree from "../../../public/images/kseniia-jin-n5f0INq5uKI-unsplash.jpg"
 
 
 const Hero = () => {
   return (
 <div className="hero min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="image-container max-w-sm rounded-lg shadow-2xl" />
-    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="image-container max-w-sm rounded-lg shadow-2xl" />
-    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="image-container max-w-sm rounded-lg shadow-2xl" />
+    <img src={carrouselOne.src} className="image-container max-w-sm rounded-lg shadow-2xl" />
+    <img src={carrouselTwo.src} className="image-container max-w-sm rounded-lg shadow-2xl" />
+    <img src={carrouselThree.src} className="image-container max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold text-white">Box Office News!</h1>
       <p className="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import bannerImage from "../../public/images/IMG_1190.jpg"
+import carrouselOne from "../../public/images/IMG_1974 kopie 5.png"
 
 export default function Home() {
   return (
     <main>
-<div style={{backgroundImage: `url(${bannerImage.src})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh'}}>        <Navbar />
+<div style={{backgroundImage: `url(${carrouselOne.src})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh'}}>        <Navbar />
         <Hero />
       </div>
       <Footer />
