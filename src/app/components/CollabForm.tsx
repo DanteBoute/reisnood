969c4c52506840a06d558133c661a6d5b6c8e05b
DanteBoute <danteboute1@outlook.com>
2024-04-16 14:50:52 +0200

    CollabForm add

--- src/app/components/CollabForm.tsx
+++ src/app/components/CollabForm.tsx
@@ -1,64 +1,87 @@
 import React from "react";
 import "./CollabForm.css";
+import Carousel1 from "../../../public/images/ZwartStrandTessa.jpg";
+import Carousel2 from "../../../public/images/TessaKano.jpeg";
+import Carousel3 from "../../../public/images/TessaGeleRugzak.jpg";
+import Carousel4 from "../../../public/images/TessaRodeJas.png";
 
 const ContactForm: React.FC = () => {
   return (
     <>
-      <form className="contact-form">
-        <div className="form-group">
-          <label htmlFor="firstName">First Name</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            placeholder="Enter your first name"
-            required
-          />
+      <div className="fullDiv">
+        <div className="leftSideDiv">
+          <div className="verticalCarousel">
+            <div className=" h-96 carousel carousel-vertical rounded-box">
+              <div className="carouselImage image-container carousel-item h-full">
+                <img src={Carousel1.src} />
+              </div>
+              <div className="carouselImage image-container carousel-item h-full">
+                <img src={Carousel2.src} />
+              </div>
+              <div className="carouselImage image-container carousel-item h-full">
+                <img src={Carousel3.src} />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            placeholder="Enter your last name"
-            required
-          />
+        <div className="rightSideDiv">
+          <form className="contact-form">
+            <div className="form-group">
+              <label htmlFor="firstName">Voornaam</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder="Typ hier je voornaam"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Naam</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                placeholder="Typ hier je naam"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Typ hier je email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="subject">Onderwerp</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Typ hier je onderwerp"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Bericht</label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Typ hier jouw bericht"
+                rows={4}
+                required
+              />
+            </div>
+            <button className="btn btn-primary" type="submit">
+              Verstuur
+            </button>
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="subject">Subject</label>
-          <input
-            type="text"
-            id="subject"
-            name="subject"
-            placeholder="Enter the subject"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            placeholder="Enter your message"
-            rows={4}
-            required
-          />
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
-      </form>
+      </div>
     </>
   );
 };
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
-  <nav className="grid grid-flow-col gap-4">
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Samenwerking</a>
-  </nav> 
-  <nav>
-    <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-    </div>
-  </nav> 
-  <aside>
-    <p>Copyright © 2024 - All right reserved by Reisnood.be</p>
-  </aside>
-</footer>
-  )
-}
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Samenwerking</a>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
+        </div>
+      </nav>
+      <aside>
+        <p>Copyright © 2024 - All right reserved by Reisnood.be</p>
+      </aside>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
